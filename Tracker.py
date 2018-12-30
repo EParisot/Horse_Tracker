420a0d883db82847d2f3ad48bb4dfabab808e617
eparisot <eparisot@student.42.fr>
2018-12-30 15:28:40 +0100

    Tracker motor

--- Tracker.py
+++ Tracker.py
@@ -1,4 +1,5 @@
 from pivideostream import PiVideoStream
+from Step_Motor import Step_Motor
 from PIL import Image
 from keras.models import load_model
 import numpy as np
@@ -13,6 +14,10 @@ print("Model loaded")
 # create a threaded video stream, allow the camera sensor to warmup
 vs = PiVideoStream().start()
 time.sleep(2.0)
+
+motor = Step_Motor()
+step = 16
+delay = 0.001
 
 #test
 try:
@@ -33,7 +38,15 @@ while True:
     pred = model.predict(image)
     pred = np.argmax(pred)
     
-    #TODO Motor drive
-    
+    # Motor drive
+    if pred == 1:
+        motor.clockwise(step, delay)
+    elif pred == 2:
+        motor.clockwise(2 * step, delay)
+    elif pred == 3:
+        motor.counter_clockwise(step, delay)
+    elif pred == 4:
+        motor.counter_clockwise(2 * step, delay)
+        
 vs.stop()
 print("Stop")
