0625e31a6afaa6722db6061a37893dc6c5a588ca
eparisot <eparisot@student.42.fr>
2018-12-30 15:20:55 +0100

    Signed-off-by: eparisot <eparisot@student.42.fr>

--- Tracker.py
+++ Tracker.py
@@ -5,22 +5,14 @@ import numpy as np
 import sys
 import time
 from const import *
-
+    
 #Load model
-if len(sys.argv) > 1:
-    model = load_model(sys.argv[1])
-    print("Model loaded")
-else:
-    print("Error : No model specified")
-    exit(0)
+model = load_model("model.h5")
+print("Model loaded")
 
 # create a threaded video stream, allow the camera sensor to warmup
 vs = PiVideoStream().start()
 time.sleep(2.0)
-
-# Define the codec and create VideoWriter object
-fourcc = cv2.VideoWriter_fourcc(*'XVID')
-out = cv2.VideoWriter(str(time.time()) + '.avi',fourcc, 20.0, RESOLUTION)
 
 #test
 try:
@@ -35,17 +27,13 @@ print("Tracker Started")
 while True:
     # grab the frame from the threaded video stream 
     frame = vs.read()
-    image = cv2.resize(frame, (160, 96))
-    image = np.array([image]) / 255.0
+    image = np.array([frame]) / 255.0
+    
     # Model prediction
     pred = model.predict(image)
     pred = np.argmax(pred)
     
     #TODO Motor drive
-
-    # Write image to video
-    out.write(frame)
     
 vs.stop()
-out.release()
 print("Stop")
