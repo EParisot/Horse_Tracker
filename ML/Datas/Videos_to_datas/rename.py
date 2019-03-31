ca8cb33f9c009d4604b46556c4021e0924f678eb
eparisot <eparisot@student.42.fr>
2019-03-31 13:13:03 +0200

    Signed-off-by: eparisot <eparisot@student.42.fr>

--- ML/Datas/Videos_to_datas/rename.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-
-for path in os.listdir("raw"):
-    directory = os.path.join("raw", path)
-    if os.path.isdir(directory):
-        for elem in os.listdir(directory):
-            if "_" not in elem:
-                os.rename(os.path.join(directory, elem), os.path.join("labelised", path + "_" + elem))
